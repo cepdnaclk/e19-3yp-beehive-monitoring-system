a49709b9967ab977d76ef41afba09a2d14725457
19Kaumini <e19009@eng.pdn.ac.lk>
2023-12-04 09:04:11 +0530

    Update Landing page
    Add text to the page

--- code/beehive-dashboard/src/Pages/Landing.jsx
+++ code/beehive-dashboard/src/Pages/Landing.jsx
@@ -12,6 +12,10 @@ function Landing() {
         <img src={Banner} alt="" className="banner-image" />
         <div className="color-overlay"></div>
       </div>
+      <div className="content-container">
+        <h3>Monitoring Hives, Nurturing Lives.</h3>
+        <p>Harvesting insights from the heartbeat of nature, our beehive monitoring system buzzes with intelligence, ensuring the sweet success of both bees and beekeepers alike.</p>
+      </div>
     </div>
   );
 }
