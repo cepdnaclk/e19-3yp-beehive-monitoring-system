577fb14cf63176c032b12683a7214f8df5463212
19Kaumini <e19009@eng.pdn.ac.lk>
2023-12-04 08:59:08 +0530

    Create Navbar

---
+++ code/beehive-dashboard/src/Components/Navbar.jsx
@@ -0,0 +1,38 @@
+import Logo from '../Assets/Logo.png';
+import SignUp from '../Assets/SignUp.png';
+import { Link } from 'react-router-dom';
+import ReorderIcon from '@mui/icons-material/Reorder';
+import { useState } from 'react';
+import '../Styles/Navbar.scss';
+
+function Navbar() {
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
+
+  return (
+    <div className='navbar'>
+      <div className='leftSide' id={openLinks ? "open" : "close"}>
+        <Link to="/"><img src={Logo} alt='Logo' /></Link>
+        <div className="hiddenLinks">
+          <Link to="/contactUs">Contact Us</Link>
+        </div>
+      </div>
+      <div className='rightSide'>
+        <Link to="/contactUs">Contact Us</Link>
+        <button className="signUpButton">
+          <Link to="/signUp">
+            Sign Up <img src={SignUp} alt="" />
+          </Link>
+        </button>
+        <button onClick={toggleNavbar}>
+          <ReorderIcon />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
