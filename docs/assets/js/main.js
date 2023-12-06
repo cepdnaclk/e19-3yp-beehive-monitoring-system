b31f89b693780e043824e863acb910e96a002ecb
19Kaumini <e19009@eng.pdn.ac.lk>
2023-12-06 14:41:33 +0530

    Add scroll up icon

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -35,7 +35,13 @@ function scrollHeader(){
 }
 window.addEventListener('scroll', scrollHeader)
 
-
+/* SHOW SCROLL UP */ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    
+    if(this.scrollY >= 400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /* DARK LIGHT THEME */ 
 const themeButton = document.getElementById('theme-button')
