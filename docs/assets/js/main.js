da20057938a77f19073637c497df41f70b20a206
19Kaumini <e19009@eng.pdn.ac.lk>
2023-12-06 14:45:35 +0530

    Update active links

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -35,6 +35,26 @@ function scrollHeader(){
 }
 window.addEventListener('scroll', scrollHeader)
 
+/* SCROLL SECTIONS ACTIVE LINK */
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
 /* SHOW SCROLL UP */ 
 function scrollUp(){
     const scrollUp = document.getElementById('scroll-up');
