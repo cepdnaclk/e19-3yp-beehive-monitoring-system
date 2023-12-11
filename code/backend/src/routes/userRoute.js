269a874e1a56a8490d87fd0f1c23a6eaed1ddd48
bimbara28 <115542059+Bimbara28@users.noreply.github.com>
2023-12-11 22:50:00 +0530

    Express

--- code/backend/src/routes/userRoute.js
+++ code/backend/src/routes/userRoute.js
@@ -1,17 +1,18 @@
-import express from 'express';
-import {createUser,getAllUsers,getUser,updateUser,deleteUser} from '../controllers/userController.js'
+import express from "express";
+import {
+  createUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/userController.js";
 
 export const router = express.Router();
 
-router.get('/user', getAllUsers);
-router.get('/user/:id', getUser);
+router.get("/user", getAllUsers);
+router.get("/user/:id", getUser);
 
-router.post('/user', createUser);
+router.post("/user", createUser);
 
-router.put('/user/:id', updateUser);
-router.delete('/user/:id', deleteUser);
-
-
-
-
-
+router.put("/user/:id", updateUser);
+router.delete("/user/:id", deleteUser);
