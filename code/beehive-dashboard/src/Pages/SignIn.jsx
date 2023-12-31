0907172de3177bd5409d9c77a75cf23d929fc81e
19Kaumini <e19009@eng.pdn.ac.lk>
2023-12-31 15:06:37 +0530

    Create Beehive Cards

--- code/beehive-dashboard/src/Pages/SignIn.jsx
+++ code/beehive-dashboard/src/Pages/SignIn.jsx
@@ -28,7 +28,7 @@ function SignIn() {
         }
         
         try {
-            const res = await fetch('http://localhost:3000/api/login', {
+            const res = await fetch('http://localhost:3000/api/users/login', {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json',
