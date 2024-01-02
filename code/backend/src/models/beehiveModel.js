fa48b75c8801e17b29cd135ae112faa6ca1b0c56
G5andeepD <e19063@eng.pdn.ac.lk>
2024-01-02 15:20:03 +0530

    beehive model commit

--- code/backend/src/models/beehiveModel.js
+++ code/backend/src/models/beehiveModel.js
@@ -15,10 +15,9 @@ const beehiveSchema = new Schema(
     },
     location: {
       type: String,
-      required: [true, "Please add the beehive location"],
     },
 
-    
+    // These Metrics represent the value of each metric at the time when the user last interacted
     CO2: {
       type: String,
     },
@@ -35,7 +34,6 @@ const beehiveSchema = new Schema(
     Battery_level: {
       type: String,
     },
-
   },
   {
     timestamps: true,
