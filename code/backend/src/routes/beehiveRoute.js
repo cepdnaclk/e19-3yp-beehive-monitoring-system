ce0ecb6e9e7c06e601d5caaf6bc6c651eb110ac1
bimbara28 <115542059+Bimbara28@users.noreply.github.com>
2024-01-05 20:16:21 +0530

    Created beehiveMetricsRoute

--- code/backend/src/routes/beehiveRoute.js
+++ code/backend/src/routes/beehiveRoute.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { validateToken } from "../middleware/validateTokenHandler.js";
 import {
-    getBeehives,
-    createBeehive,
-    getBeehive,
-    updateBeehive,
-    deleteBeehive,
+  getBeehives,
+  createBeehive,
+  getBeehive,
+  updateBeehive,
+  deleteBeehive,
 } from "../controllers/beehiveController.js";
 
 export const router = express.Router();
@@ -14,5 +14,3 @@ router.use(validateToken);
 router.route("/").get(getBeehives).post(createBeehive);
 
 router.route("/:id").get(getBeehive).put(updateBeehive).delete(deleteBeehive);
-
-
