e96f55aa414410a59b22b86854ecfb980a408185
G5andeepD <e19063@eng.pdn.ac.lk>
2024-01-06 08:27:13 +0530

    some tropuble shooting

---
+++ code/aws-lambda/beehiveMetricsModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const beehiveMetricsSchema = new Schema(
+  {
+    beehive_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Beehive",
+    },
+
+    CO2: {
+      type: String,
+    },
+    Temperature: {
+      type: String,
+    },
+    Humidity: {
+      type: String,
+    },
+    Weight: {
+      type: String,
+    },
+
+    Battery_level: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const BeehiveMetrics = mongoose.model('BeehiveMetrics', beehiveMetricsSchema);
+module.exports = { BeehiveMetrics };
