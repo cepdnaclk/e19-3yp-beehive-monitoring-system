34cf42e2e60b807a197244c5cf229cf2060e46e6
G5andeepD <e19063@eng.pdn.ac.lk>
2024-01-07 19:21:11 +0530

    logout fixed

--- code/beehive-dashboard/src/Components/NavbarBlack.jsx
+++ code/beehive-dashboard/src/Components/NavbarBlack.jsx
@@ -1,14 +1,22 @@
 import Logo from '../Assets/Logo.png';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useState,useContext } from 'react';
 import '../Styles/Components/NavbarBlack.scss';
+import { AuthContext } from '../Context/AuthContext';
+
+
 
 function NavbarBlack() {
   const [openLinks, setOpenLinks] = useState(false);
+  const { logout } = useContext(AuthContext);
 
   const toggleNavbar = () => {
     setOpenLinks(!openLinks);
   };
+
+  const handleLogout = () => {
+    logout();
+  }
 
   return (
     <div className='navb'>
@@ -17,7 +25,7 @@ function NavbarBlack() {
       </div>
       <ul className="navb-menu">
         <li className='contact'><Link to="/contact">CONTACT US</Link></li>
-        <li className='navb-signout'><Link to="/">SIGN OUT</Link></li>
+        <li className='navb-signout'><Link to="/signin">SIGN OUT</Link></li>
       </ul>
     </div>
   );
