c05d295f5729c22882d3e191b504729ffbad7e8c
bimbara28 <115542059+Bimbara28@users.noreply.github.com>
2024-01-21 18:50:42 +0530

    Beehive swagger

--- code/backend/src/routes/beehiveRoute.js
+++ code/backend/src/routes/beehiveRoute.js
@@ -9,6 +9,42 @@ import {
 } from "../controllers/beehiveController.js";
 
 export const router = express.Router();
+import swaggerJsDoc from "swagger-jsdoc";
+
+/**
+ * @swagger
+ * /api/beehive/get:
+ *   post:
+ *     summary: Get beehive information
+ *     description: This endpoint is for retrieving information about a beehive.
+ *     tags: [Beehives]
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved beehive information.
+ *         content:
+ *           application/json:
+ *             examples:
+ *               example1:
+ *                 value:
+ *                   Name: Beehive1
+ *                   Location: Location1
+ *                   CO2: 100
+ *                   Temperature: 20
+ *                   Humidity: 30
+ *                   Weight: 40
+ *                   Battery_level: 50
+ *               example2:
+ *                 value:
+ *                   Name: Beehive2
+ *                   Location: Location2
+ *                   CO2: 80
+ *                   Temperature: 25
+ *                   Humidity: 40
+ *                   Weight: 45
+ *                   Battery_level: 60
+ */
+
+
 
 router.use(validateToken);
 router.route("/").get(getBeehives).post(createBeehive);
