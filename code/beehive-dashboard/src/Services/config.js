28b3878d438aa01b2ef4c2fd0edb37bb77d001af
G5andeepD <e19063@eng.pdn.ac.lk>
2025-01-21 21:17:24 +0530

    add url

--- code/beehive-dashboard/src/Services/config.js
+++ code/beehive-dashboard/src/Services/config.js
@@ -1,3 +1,3 @@
-export const PARENT_API_URL = 'http://localhost:5001/api';
+export const PARENT_API_URL = 'http://beehive-backend-1889726403:5000/api';
 export const PARENT_API_URL_2 = 'http://beehive-backend.eba-suumbhuc.ap-southeast-1.elasticbeanstalk.com/api';
 // export const PARENT_API_URL2 = 'http://beehive-backend.eba-xkm8nww8.us-west-2.elasticbeanstalk.com/api';
